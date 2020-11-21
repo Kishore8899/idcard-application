3574c19ccf93121cde1c16dfea2a8e478036dd6b
Kishore kumar k <40658697+Kishore8899@users.noreply.github.com>
2020-11-21 22:38:15 +0530

    Update mailing.php

--- mailing.php
+++ mailing.php
@@ -1,7 +1,7 @@
 <?php
 include 'test.php';
 $to_mail="kish16124.it@rmkec.ac.in";
-$mail_content="eppadi da irukka kishore";
+$mail_content="how are you?";
 $mail_subject="hi";
  send_mqil_using_php_mailer($to_mail,$mail_content,$mail_subject)
 ;
